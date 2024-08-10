f887ec7c8b075310fe32cde0a45865c7a470ead2
Trần Ngọc Oanh <23730431.oanh@student.iuh.edu.vn>
2024-08-10 21:23:47 +0700

    Delete Welcome.java

--- Welcome.java
+++
@@ -1,6 +0,0 @@
-public class  Welcome {
-    public static void main(String[] args) {
-        System.out.println("Hello Tran Ngoc Oanh!");
-        System.out.println("Welcome to DHKTPM19B!");
-    }
-}
